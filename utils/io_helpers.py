f16a7d47cadc6c06ad8f4c313d07de2cf02e146a
christocar <Christoffer.Carlsson88@gmail.com>
2026-05-27 14:34:30 +0000

    Add bronze ingestion notebook with column mapping support

--- utils/io_helpers.py
+++ utils/io_helpers.py
@@ -15,10 +15,14 @@ def read_csv_from_volume(spark: SparkSession, path: str, header=True, infer_sche
 
 def write_df_to_table(df: DataFrame, table_name: str, mode="overwrite") -> None:
     # overwriteSchema lets us change columns when re-running during development
+    # column mapping lets us use names with spaces/slashes (needed for bronze)
     (
         df.write
         .mode(mode)
         .option("overwriteSchema", "true")
+        .option("delta.columnMapping.mode", "name")
+        .option("delta.minReaderVersion", "2")
+        .option("delta.minWriterVersion", "5")
         .saveAsTable(table_name)
     )
 
